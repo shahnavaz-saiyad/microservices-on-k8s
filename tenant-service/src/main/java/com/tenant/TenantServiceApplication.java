ca0bb7d88efbc6f6e866eaa1b663d5a3ece870b7
Shahnavaz Saiyad <ssaiyed173@gmail.com>
2024-04-08 10:36:48 +0530

    Updated config credentials with ENV

--- tenant-service/src/main/java/com/tenant/TenantServiceApplication.java
+++ tenant-service/src/main/java/com/tenant/TenantServiceApplication.java
@@ -1,14 +1,9 @@
 package com.tenant;
 
-import com.common.CommonApplication;
-import com.common.repository.tenant.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
